2ff488740b9c77aa8c00a96c6e10383922181173
HeavyLaser-Sparrow <srineeth.sana@gmail.com>
2024-09-14 12:30:45 -0700

    Create Navy.java

---
+++ Navy.java
@@ -0,0 +1,43 @@
+public class Navy{
+	private boolean atSea;
+	private int health;
+	private boolean enemy;
+	
+	public Navy(int asdf){
+		atSea = false;
+		health = asdf;
+		enemy = false;
+	}
+	
+
+	public void setSail(){
+		atSea = true;
+		System.out.println("Setting Sail Now");
+	}
+	public void dock(int max){
+		if(atSea){
+			System.out.println("Docking Now");
+			atSea = false;
+			health = max;
+			enemy = false;
+		} else {
+			System.out.println("You are already at a dock");
+		}
+		
+		int prb = (int)(Math.random() * 10) + 1; // 1-10
+		if(prb > 0){
+			enemy = true;
+			System.out.println("Oh no! An enemy approaches.");
+			System.out.println("SHOOT THEM!!!!");
+		} 
+	
+	}	
+	public boolean getSea(){return atSea;}
+	public int getHealth(){return health;}
+	public boolean getEnemy(){return enemy;}
+	public void subHealth(int x){
+		health -= x;
+	}
+	public void ridEnemy(){enemy = false;}
+
+}
